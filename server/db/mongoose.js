eedfc13b1858cacd09c3bc62b561164c3818f285
Sabry Mohamed <Sabry@pere.com>
2022-02-28 10:50:31 +0200

    dbname

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,9 +3,9 @@ mongoose.Promise = global.Promise;
 
 const dbName = 'TodoApp';
 
-const uri = `mongodb+srv://sabri:OD2o0GOGtXoeuPil@cluster0.znry1.mongodb.net/TodoApp?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://sabri:OD2o0GOGtXoeuPil@cluster0.znry1.mongodb.net/${dbName}?retryWrites=true&w=majority`;
 const url = 'mongodb://localhost:27017/' + dbName;
-mongoose.connect(uri || url);
+mongoose.connect(uri);
 
 module.exports = {
     mongoose
