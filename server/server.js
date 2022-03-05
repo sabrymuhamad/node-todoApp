d4634d0cf250f1321935d891ef21548cd7fb4c4c
Sabry Mohamed <Sabry@pere.com>
2022-03-05 14:36:52 +0200

    hashing

--- server/server.js
+++ server/server.js
@@ -101,10 +101,10 @@ app.patch('/todos/:id', (req, res) => {
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
-    user.save().then((user) => {
-        res.send(user);
-    }, (err) => {
-        res.status(400).send(err);
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
     }).catch((e) => {
         res.status(400).send(e);
     });
